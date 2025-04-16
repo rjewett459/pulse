8f99b962e8f9e25b97f34af2fd748dc0ab28d9ea
rjewett459 <Info@chatsites.io>
2025-04-16 11:12:16 -0700

    fix: allow useSearchParams by disabling static rendering

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import React from "react";
 import { TranscriptProvider } from "@/app/contexts/TranscriptContext";
 import { EventProvider } from "@/app/contexts/EventContext";
