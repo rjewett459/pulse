59398e71d6c6e969d62cb7d521c1b23383e9480d
rjewett459 <Info@chatsites.io>
2025-04-17 16:16:48 -0700

    🧼 Remove unused sessionStatus from BottomToolbar props

--- src/app/components/BottomToolbar.tsx
+++ src/app/components/BottomToolbar.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { SessionStatus } from "@/app/types";
 
 interface BottomToolbarProps {
-  sessionStatus: SessionStatus;
   isPTTUserSpeaking: boolean;
   handleTalkButtonDown: () => void;
   handleTalkButtonUp: () => void;
@@ -13,7 +12,6 @@ interface BottomToolbarProps {
 }
 
 function BottomToolbar({
-  sessionStatus,
   isPTTUserSpeaking,
   handleTalkButtonDown,
   handleTalkButtonUp,
